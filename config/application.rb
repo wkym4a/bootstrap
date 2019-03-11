ec76c80ed90e914c47a7f52b225e18f29f59b69c
若山侑輝 <wkym_4a@icloud.com>
2019-03-11 13:20:05 +0900

    herokuに上げられないのでコミットし直す

---
+++ config/application.rb
@@ -0,0 +1,18 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Bootstrap
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
+    #追記
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+  end
+end
